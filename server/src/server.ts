4b56e10af9f5a7d5de2a7a75b1a95ac6636f89fa
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-01-24 19:21:19 -0300

    create: create back-end setup

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber',
+      }
+    }
+  })
+
+  return habits
+})
+
+app.listen({
+  port:3333
+}).then(() => {
+  console.log('Server is running!');
+  
+})
