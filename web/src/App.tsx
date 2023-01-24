415a0600fe0ffcad784770c5f1f05098e3430594
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-01-24 20:19:26 -0300

    create: create front-end setup

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+import './styles/global.css'
+
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={20} />
+      <Habit completed={30}/>
+      <Habit />
+      <Habit />
+      <Habit />
+      <Habit />
+    </div>
+  )
+}
+
+export default App
