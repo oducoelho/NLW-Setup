f1dea2440879a6ce36145ba86490cfb0dcc9594f
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-01-25 19:10:42 -0300

    creation of new tables and performing the first routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,12 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './lib/routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber',
-      }
-    }
-  })
-
-  return habits
-})
 
 app.listen({
   port:3333
