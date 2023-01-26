deb6f6ce390fbdc0e94746f937038442a01e7db5
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-01-26 18:28:45 -0300

    feat: finish back-end

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,4 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = [
@@ -9,6 +10,12 @@ const weekDays = [
   'S',
   'S',
 ]
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 
 export const SummaryTable = () => {
   return (
@@ -27,34 +34,20 @@ export const SummaryTable = () => {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map(date => {
+          return (
+            <HabitDay key={date.toString()} />
+          )
+        })} 
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+          return (
+          <div 
+            key={i} 
+            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" 
+          />
+          )
+        })}
       </div>
     </div>
   )
--- web/src/utils/generate-dates-from-year-beginning.ts
+++ web/src/utils/generate-dates-from-year-beginning.ts
@@ -4,4 +4,13 @@ export function generateDatesFromYearBeginning() {
   const firstDayOfTheYear = dayjs().startOf('year')
   const today = new Date()
 
+  const dates = []
+  let compareDate = firstDayOfTheYear
+
+  while (compareDate.isBefore(today)) {
+    dates.push(compareDate.toDate())
+    compareDate = compareDate.add(1, 'day')
+  }
+
+  return dates
 }
